c05ba22108ec340a6a37589e799057b82901e0ce
Sam Perozek <samperozek@gmail.com>
2022-06-11 11:45:33 -0500

    commit tree f8e17e2f98f75574b0e0b55814aa5f6dc014a77e

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+// // Add your code here
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json",
+//     },
+//     body: JSON.stringify({
+//       dogName: "Byron",
+//       dogBreed: "Poodle",
+//     }),
+//   };
+  
+//   fetch("http://localhost:3000/dogs", configurationObject);
+
+
+function submitData(userName, userEmail){
+    fetch("http://localhost:3000/users",{
+        method:"POST",
+        headers: {
+            "Content-Type": "applciation/json",
+            "Accept": "application/json",
+        }
+    })
+}
